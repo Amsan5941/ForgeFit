47a6e62a3c800e6ec87266218f73714bc2a6a8d4
Mithuusan Kirupananthan <141945465+MithuusanK@users.noreply.github.com>
2025-01-24 16:22:35 -0500

    Updated NavBar

---
+++ src/Components/Navbar/Hamburger.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+
+const Hamburger = () => {
+  return (
+    <div>
+        <div className='hamburger'>
+            <div className="burger 1" />
+            <div className="burger 2" />
+            <div className="burger 3" />
+            
+        </div>
+      
+    </div>
+  )
+}
+
+export default Hamburger
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import Hamburger from './Hamburger'
 import './Navbar.css'
 
 const Navbar = () => {
   return (
     <div>
-      <p>LOL</p>
-      
+      <div className="navigation">
+        <ul>
+          <li>Home</li>
+          <li>Tracker</li>
+          <li>Gym Bot</li>
+          <li>About</li>
+          <li>Contact Us</li>
+        </ul>
+          <div className='hamburger'>
+            <Hamburger />
+          </div>
+
+
+      </div>
     </div>
   )
 }
