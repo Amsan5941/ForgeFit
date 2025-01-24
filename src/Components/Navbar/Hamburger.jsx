d59b63643fdc669a2fd298f9ec28d9baa7569288
Mithuusan Kirupananthan <141945465+MithuusanK@users.noreply.github.com>
2025-01-24 16:57:45 -0500

    Updated Navbar

--- src/Components/Navbar/Hamburger.jsx
+++ src/Components/Navbar/Hamburger.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Navbar.css'
 
+
 const Hamburger = () => {
   return (
     <div>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Hamburger from './Hamburger'
 import './Navbar.css'
 
