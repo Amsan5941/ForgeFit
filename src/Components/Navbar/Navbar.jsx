4bc09d9e505c9cdbe12207255aee32ced54bdd47
Mithuusan Kirupananthan <141945465+MithuusanK@users.noreply.github.com>
2025-01-27 18:50:20 -0500

    Updated NavBar options

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -19,7 +19,8 @@ const Navbar = () => {
         <div className={`navbar-links ${isOpen ? "active" : ""}`}>
           <a href="#home">Home</a>
           <a href="#about">About</a>
-          <a href="#services">Services</a>
+          <a href="tracker">Tracker</a>
+          <a href="chat">Chat Bot</a>
           <a href="#contact">Contact</a>
         </div>
       </div>
