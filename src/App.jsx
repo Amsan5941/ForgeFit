dd4ea969ab88c769913cf50b9a9b331497f25b80
Mithuusan Kirupananthan <141945465+MithuusanK@users.noreply.github.com>
2025-05-08 20:37:39 -0400

    Configured the routing for all the wwebsites pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import LoginButton from './Components/Logins/LoginButton';
 import LoginPage from './Components/Logins/LoginPage';
-import Write from './Components/Write';
+import About from "./Components/About/About";
+import Tracker from "./Components/Tracker/Tracker"
+import ChatBot from "./Components/ChatBot/Chat"
+import Contact from "./Components/Contact/Contact"
 
 const App = () => {
   return (
@@ -12,6 +15,11 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LoginButton />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/about" element={<About />}/>
+        <Route path="/tracker" element={<Tracker />}/>
+        <Route path="/chat" element={<ChatBot />}/>
+        <Route path="/Contact" element={<Contact />}/>
+
         
       </Routes>
 
---
+++ src/Components/ChatBot/Chat.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Chat = () => {
+  return (
+    <div>
+      <h1>Our Chat Bot offers tailored workout help based on your needs!</h1>
+    </div>
+  )
+}
+
+export default Chat
