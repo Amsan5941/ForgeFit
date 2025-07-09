98b749db8eaf3f9b836cfa55db3d5ae4f8936f1c
Amsan Naheswaran <amsan5941@gmail.com>
2025-07-09 18:53:32 -0400

    Revert "Merge branch 'miths-branch'" Reverting to this branch since sign is broken This reverts commit 9183bd4ffb92a3f69da8a523068dd899915727e3, reversing changes made to 719fc4b73bf2ed192cbf0002ef3686ea0f2bb941.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
+import LoginButton from './Components/Logins/LoginButton';
 import LoginPage from './Components/Logins/LoginPage';
 import About from "./Components/About/About";
 import Tracker from "./Components/Tracker/Tracker";
@@ -14,13 +15,13 @@ const App = () => {
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/" element={<LoginButton />} />
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/tracker" element={<Tracker />} />
-        <Route path="/chat" element={<ChatBot />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />}/>
+        <Route path="/tracker" element={<Tracker />}/>
+        <Route path="/chat" element={<ChatBot />}/>
+        <Route path="/Contact" element={<Contact />}/>
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </div>
--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -3,17 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      <section className="hero-contact">        
-        <h1>Meet our Team</h1>
-      </section>
-
-      <section className="about-us">
-        <h1>We are recent Universities graduates with a passion for fitness.</h1>
-      </section>
-
-      <section className="contact">
-        
-      </section>
+      <h1>Our Contact Information</h1>
     </div>
   )
 }
